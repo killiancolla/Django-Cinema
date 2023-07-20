ac5da15c7697c56f1cd94836c28d8d7df9f44e18
Jeremy-BD <jeremy.boisnard16@gmail.com>
2023-07-20 12:52:17 +0200

    fix signup/login css

--- client/src/pages/inscription/Inscription.js
+++ client/src/pages/inscription/Inscription.js
@@ -2,39 +2,39 @@ import "./incription.css";
 
 export default function Inscription() {
   return (
-    <div id="TEST">
-      <div className="main">
-        <input type="checkbox" id="chk" aria-hidden="true" />
+    <div className="login-signup-body">
+      <div className="login-signup-main">
+        <input className="signup-input" type="checkbox" id="chk" aria-hidden="true" />
         <div className="signup">
           <form>
-            <label for="chk" aria-hidden="true">
+            <label className="signup-label" htmlFor="chk" aria-hidden="true">
               Sign up
             </label>
-            <input type="text" name="txt" placeholder="User name" required="" />
-            <input type="email" name="email" placeholder="Email" required="" />
-            <input
+            <input className="signup-input" type="text" name="txt" placeholder="User name" required="" />
+            <input className="signup-input" type="email" name="email" placeholder="Email" required="" />
+            <input className="signup-input"
               type="password"
               name="pswd"
               placeholder="Password"
               required=""
             />
-            <button>Sign up</button>
+            <button className="signup-button">Sign up</button>
           </form>
         </div>
 
         <div className="login">
           <form>
-            <label for="chk" aria-hidden="true">
+            <label className="login-label" htmlFor="chk" aria-hidden="true">
               Login
             </label>
-            <input type="email" name="email" placeholder="Email" required="" />
-            <input
+            <input className="login-input" type="email" name="email" placeholder="Email" required="" />
+            <input className="login-input"
               type="password"
               name="pswd"
               placeholder="Password"
               required=""
             />
-            <button>Login</button>
+            <button className="login-button">Login</button>
           </form>
         </div>
       </div>
